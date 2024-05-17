b48c6bb5c81ce4e468b19883b9958ade7a11c0f9
saprodan <111009303+saprodan@users.noreply.github.com>
2024-05-17 11:04:31 +0300

    Update next.config.mjs
    Signed-off-by: saprodan <111009303+saprodan@users.noreply.github.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const repo = 'card-shop'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 const nextConfig = {
   // basePath: "/card-shop",
-  assetPrefix: './',
+  // assetPrefix: './',
+  
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   output: 'export',
+  
   images: {
     unoptimized: true,
     remotePatterns: [
